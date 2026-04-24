51832a740b0cb74f02edde5c95858cbdda7a5667
asier <asier.hr15@gmail.com>
2026-04-24 18:46:21 +0200

    tests arreglados

--- src/test/java/com/asier/SistemaReservas/airlineEmployees/objects/AirlineEmployeesObjects.java
+++ src/test/java/com/asier/SistemaReservas/airlineEmployees/objects/AirlineEmployeesObjects.java
@@ -1,26 +1,27 @@
 package com.asier.SistemaReservas.airlineEmployees.objects;
 
 import com.asier.SistemaReservas.airline.AirlineObjects;
+import com.asier.SistemaReservas.airline.domain.entity.AirlineEntity;
 import com.asier.SistemaReservas.airline.employee.domain.entity.AirlineEmployeeInfoEntity;
 import com.asier.SistemaReservas.airline.employee.domain.enums.EmployeeType;
 import com.asier.SistemaReservas.airport.objects.AirportObjects;
 import com.asier.SistemaReservas.users.objects.UserObjects;
 
 public class AirlineEmployeesObjects {
-    public static AirlineEmployeeInfoEntity airlineEmployeeInfo1(){
+    public static AirlineEmployeeInfoEntity airlineEmployeeInfo1(AirlineEntity airline){
         return AirlineEmployeeInfoEntity.builder()
                 .id(null)
                 .airport(AirportObjects.airport())
-                .airline(AirlineObjects.airline1())
+                .airline(airline)
                 .type(EmployeeType.WORKER)
                 .user(UserObjects.airportEmployee())
                 .build();
     }
-    public static AirlineEmployeeInfoEntity airlineEmployeeInfo2(){
+    public static AirlineEmployeeInfoEntity airlineEmployeeInfo2(AirlineEntity airline){
         return AirlineEmployeeInfoEntity.builder()
                 .id(null)
                 .airport(AirportObjects.airport())
-                .airline(AirlineObjects.airline1())
+                .airline(airline)
                 .type(EmployeeType.ADMIN)
                 .user(UserObjects.airportEmployee1())
                 .build();
--- src/test/java/com/asier/SistemaReservas/flight/objects/FlightObjects.java
+++ src/test/java/com/asier/SistemaReservas/flight/objects/FlightObjects.java
@@ -1,6 +1,8 @@
 package com.asier.SistemaReservas.flight.objects;
 
 import com.asier.SistemaReservas.airline.AirlineObjects;
+import com.asier.SistemaReservas.airline.domain.entity.AirlineEntity;
+import com.asier.SistemaReservas.airlineEmployees.objects.AirlineEmployeesObjects;
 import com.asier.SistemaReservas.airport.objects.AirportObjects;
 import com.asier.SistemaReservas.flight.domain.entity.FlightEntity;
 import com.asier.SistemaReservas.seats.objects.SeatObjects;
@@ -11,6 +13,7 @@ import java.util.List;
 
 public class FlightObjects {
     public static FlightEntity flight1(){
+        AirlineEntity airline = AirlineObjects.airline1(List.of());
         return FlightEntity.builder()
                 .id(null)
                 .origin(AirportObjects.airport())
@@ -18,7 +21,7 @@ public class FlightObjects {
                 .flightDay(LocalDate.of(2025, 3, 15))
                 .departureTime(LocalTime.of(10,30))
                 .arrivalTime(LocalTime.of(11,45))
-                .airline(AirlineObjects.airline1())
+                .airline(airline)
                 .seats(List.of(SeatObjects.seat1(),SeatObjects.seat2(),SeatObjects.seat3()))
                 .build();
     }
