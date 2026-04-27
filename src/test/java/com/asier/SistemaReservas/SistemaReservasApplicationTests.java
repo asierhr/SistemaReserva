db453351d2d906f6b11b86ffc4ed1cde5201be65
asier <asier.hr15@gmail.com>
2026-04-27 17:34:25 +0200

    tests arreglados

--- src/test/java/com/asier/SistemaReservas/SistemaReservasApplicationTests.java
+++ src/test/java/com/asier/SistemaReservas/SistemaReservasApplicationTests.java
@@ -1,11 +1,14 @@
 package com.asier.SistemaReservas;
 
+import com.asier.SistemaReservas.config.DotenvInitializer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
 @ActiveProfiles("local-test")
+@ContextConfiguration(initializers = DotenvInitializer.class)
 class SistemaReservasApplicationTests {
 
 	@Test
